ee46f5b363b5f3814994f074f2639b4c7c16437d
Markus Kraus <markus.kraus@anzing.net>
2023-05-01 21:53:40 +0200

    HtmlExportTemplate

---
+++ Model.Tests/HtmlExportTests.vb
@@ -0,0 +1,27 @@
+﻿<TestClass()>
+Public Class HtmlExportTests
+
+    <TestMethod>
+    Public Sub ToStringTest()
+        ' Arrange
+        Dim tournament = New Tournament()
+        Dim team1 = New Team(1)
+        Dim team2 = New Team(2)
+        tournament.AddTeam(team1)
+        tournament.AddTeam(team2)
+        tournament.MatchManager.Generate({team1, team2})
+        Dim matchResult = New Result()
+        matchResult.AddSetResult(New SetResult() With {.Home = 5, .Guest = 3})
+        tournament.MatchManager.SetStatus(1, matchResult)
+        Dim testClass = New HtmlExport(tournament)
+
+        ' Act
+        Dim result = testClass.ToString()
+
+        ' Assert
+        StringAssert.StartsWith(result, "{% extends 'base.html.twig' %}")
+        StringAssert.Contains(result, "<h1>Club-Liga")
+        StringAssert.EndsWith(result, "{% endblock %}")
+    End Sub
+
+End Class
--- Model/Export/HtmlExport.vb
+++ Model/Export/HtmlExport.vb
@@ -8,23 +8,8 @@ Public Class HtmlExport
         Me.Tournament = tournament
     End Sub
 
-    Private Sub AddStandings(builder As StringBuilder)
-
-        For Each standing As Standing In Tournament.GetStandings()
-            ' <tr><td>1.</td><td>Florian Kapfhammer</td><td>3</td><td class="font-weight-bold">7</td></td><td>10:2</td></tr>
-            builder.Append("<tr>")
-            builder.Append($"<td>{standing.Place}</td>")
-            builder.Append($"<td>{standing.Team.Name}</td>")
-            builder.Append($"<td>{standing.MatchCount}</td>")
-            builder.Append($"<td class=""font-weight-bold"">{standing.Points}</td>")
-            builder.Append($"<td>{standing.Goals}:{standing.GoalsAgainst}</td>")
-            builder.Append("</tr>")
-            builder.Append(Environment.NewLine)
-        Next standing
-    End Sub
-
     Public Overrides Function ToString() As String
-        Dim builder As New StringBuilder()
+        Dim builder As New StringBuilder(My.Resources.HtmlExportTemplate)
 
         AddStandings(builder)
 
@@ -32,4 +17,21 @@ Public Class HtmlExport
         Return builder.ToString()
     End Function
 
+    Private Sub AddStandings(builder As StringBuilder)
+        Dim standingsBuilder As New StringBuilder()
+
+        For Each standing As Standing In Tournament.GetStandings()
+            ' <tr><td>1.</td><td>Florian Kapfhammer</td><td>3</td><td class="font-weight-bold">7</td></td><td>10:2</td></tr>
+            standingsBuilder.Append("<tr>")
+            standingsBuilder.Append($"<td>{standing.Place}</td>")
+            standingsBuilder.Append($"<td>{standing.Team.Name}</td>")
+            standingsBuilder.Append($"<td>{standing.MatchCount}</td>")
+            standingsBuilder.Append($"<td class=""font-weight-bold"">{standing.Points}</td>")
+            standingsBuilder.Append($"<td>{standing.Goals}:{standing.GoalsAgainst}</td>")
+            standingsBuilder.Append("</tr>")
+            standingsBuilder.Append(Environment.NewLine)
+        Next standing
+
+        builder.Replace("<!-- Tabelle -->", standingsBuilder.ToString())
+    End Sub
 End Class
