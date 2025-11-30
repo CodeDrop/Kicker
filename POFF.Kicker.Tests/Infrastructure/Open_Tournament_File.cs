dfa8c0c5a1ab436b16792df7fda4cb06197810b6
Markus K. <markus-kraus@outlook.com>
2025-11-30 21:01:45 +0100

    introduce TournamentFile

---
+++ POFF.Kicker.Tests/Infrastructure/Open_Tournament_File.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using POFF.Kicker.Infrastructure;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace POFF.Kicker.Tests.Infrastructure
+{
+    [TestFixture]
+    class Open_Tournament_File
+    {
+        private TournamentFile _sut;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            using var reader = new StreamReader(@"Infrastructure\TournamentFile.xml");
+            var objectGenerator = new XmlSerializer(typeof(TournamentFile));
+            _sut = (TournamentFile)objectGenerator.Deserialize(reader);
+            reader.Close();
+        }
+
+        [Test]
+        public void File_contains_teams()
+        {
+            Assert.That(_sut.Teams, Is.Not.Empty);
+            Assert.That(_sut.Teams, Has.Length.EqualTo(3));
+        }
+
+        [Test]
+        public void File_contains_matches()
+        {
+            Assert.That(_sut.Matches, Is.Not.Empty);
+        }
+    }
+}
---
+++ POFF.Kicker.Tests/Infrastructure/Save_Tournament_File.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using POFF.Kicker.Domain;
+using POFF.Kicker.Infrastructure;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace POFF.Kicker.Tests.Infrastructure
+{
+    [TestFixture]
+    class Save_Tournament_File
+    {
+        private const string FILENAME = "unittest.xml";
+        private TournamentFile _sut;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Team[] teams =
+                [
+                    new Team { Name = "Team A" },
+                    new Team { Name = "Team B" }
+                ];
+
+            _sut = new TournamentFile
+            {
+                Teams = teams,
+                Matches = [new Match(1, teams[0], teams[1])]
+            };
+
+            var writer = new StreamWriter(FILENAME, false);
+
+            var serializer = new XmlSerializer(typeof(TournamentFile));
+            serializer.Serialize(writer, _sut);
+            writer.Close();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            if (File.Exists(FILENAME))
+            {
+                File.Delete(FILENAME);
+            }
+        }
+
+        [Test]
+        public void Saved_file_exists()
+        {
+            Assert.That(FILENAME, Does.Exist);
+        }
+    }
+}
