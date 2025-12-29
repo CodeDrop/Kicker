db78804c786870fce5cf3afda4c061d3c9b79428
Markus K. <markus-kraus@outlook.com>
2025-12-29 22:51:33 +0100

    introduce TwigFileInjectionExporter

---
+++ POFF.Meet.Tests/Infrastructure/Export_into_twig_file.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using POFF.Meet.Domain;
+using POFF.Meet.Infrastructure;
+using POFF.Meet.View.Model;
+using System.IO;
+
+namespace POFF.Meet.Tests.Infrastructure;
+
+[TestFixture]
+class Export_into_twig_file
+{
+    private Tournament _tournament;
+    private TwigFileInjectionExporter _sut;
+    private string _targetFile;
+    private string _content;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        _targetFile = Path.GetTempFileName();
+        File.Copy(@"Infrastructure\TournamentPage.html.twig", _targetFile, overwrite: true);
+        var storage = new FileTournamentStorage(@"Infrastructure\TournamentFile.xml");
+        _tournament = storage.Load();
+        _sut = new TwigFileInjectionExporter(_targetFile);
+        _sut.Export(_tournament);
+        _content = File.ReadAllText(_targetFile);
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        if (File.Exists(_targetFile))
+        {
+            File.Delete(_targetFile);
+        }
+    }
+
+    [Test]
+    [TestCase("<-- POFF.Meet#Ranking-Start -->")]
+    [TestCase("<-- POFF.Meet#Ranking-End -->")]
+    public void File_contains_ranking_tag(string tag)
+    {
+        Assert.That(_content, Does.Contain(tag));
+    }
+
+    [Test]
+    [TestCase("<tr><td>1</td><td>Team A</td><td>0</td><td class=\"font-weight-bold\">0</td>")]
+    public void File_contains_ranking(string ranking)
+    {
+        Assert.That(_content, Does.Contain(ranking));
+    }
+
+    [Test]
+    public void File_contains_all_team_names()
+    {
+        foreach (Team team in _tournament.Teams)
+        {
+            Assert.That(_content, Does.Contain(team.Name));
+        }
+    }
+}
---
+++ POFF.Meet/Infrastructure/TwigFileInjectionExporter.cs
@@ -0,0 +1,50 @@
+﻿using POFF.Meet.Domain.ScoreModes;
+using POFF.Meet.View.Model;
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace POFF.Meet.Infrastructure;
+
+public class TwigFileInjectionExporter
+{
+    private readonly string _targetFilename;
+
+    public TwigFileInjectionExporter(string targetFilename)
+    {
+        _targetFilename = targetFilename;
+    }
+
+    public void Export(Tournament tournament)
+    {
+        var content = File.ReadAllText(_targetFilename);
+        var ranking = GetRankingHtml(tournament);
+        content = Regex.Replace(content,
+            // capture start tag, inner content (non-greedy, singleline), and end tag
+            "(<-- POFF\\.Meet#Ranking-Start -->)(.*?)(<-- POFF\\.Meet#Ranking-End -->)",
+            m => m.Groups[1].Value + ranking + m.Groups[3].Value,
+            RegexOptions.Singleline, Regex.InfiniteMatchTimeout);
+        File.WriteAllText(_targetFilename, content);
+    }
+
+    private string GetRankingHtml(Tournament tournament)
+    {
+        var standingsBuilder = new StringBuilder();
+
+        foreach (Standing standing in tournament.GetStandings())
+        {
+            // <tr><td>1.</td><td>Spieler 1</td><td>3</td><td class="font-weight-bold">7</td></td><td>10:2</td></tr>
+            standingsBuilder.Append("<tr>");
+            standingsBuilder.Append($"<td>{standing.Place}</td>");
+            standingsBuilder.Append($"<td>{standing.Team.Name}</td>");
+            standingsBuilder.Append($"<td>{standing.MatchCount}</td>");
+            standingsBuilder.Append($"<td class=\"font-weight-bold\">{standing.Points}</td>");
+            standingsBuilder.Append($"<td>{standing.Goals}:{standing.GoalsAgainst}</td>");
+            standingsBuilder.Append("</tr>");
+            standingsBuilder.Append(Environment.NewLine);
+        }
+
+        return standingsBuilder.ToString();
+    }
+}
