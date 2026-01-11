99cd13bceaa190786c49aab19f41219db56e4857
Markus K. <markus-kraus@outlook.com>
2026-01-11 12:00:36 +0100

    add Swiss System play mode

---
+++ POFF.Meet/Domain/PlayModes/SwissSystem/SwissSystemPlayMode.cs
@@ -0,0 +1,15 @@
+﻿using POFF.Meet.Domain.PlayModes;
+using System.Collections.Generic;
+
+namespace POFF.Meet;
+
+public class SwissSystemPlayMode : PlayMode
+{
+    public SwissSystemPlayMode() : base("Swiss System")
+    { }
+
+    public override IEnumerable<Fixture> Generate(int teamsCount)
+    {
+        return [];
+    }
+}
