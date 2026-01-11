7a1564494ca76297998af84be521af75eba80ff0
Markus K. <markus-kraus@outlook.com>
2026-01-11 11:31:36 +0100

    update copyright year

--- POFF.Meet/AssemblyInfo.cs
+++ POFF.Meet/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("POFF Meet")]
-[assembly: AssemblyDescription("Tournament Manamgement")]
+[assembly: AssemblyDescription("Tournament Management")]
 [assembly: AssemblyCompany("POFF")]
 [assembly: AssemblyProduct("Meet")]
-[assembly: AssemblyCopyright("©2004-2025 Programs Of Free Fantasy")]
+[assembly: AssemblyCopyright("©2004-2026 Programs Of Free Fantasy")]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.1.1.1005")]
+[assembly: AssemblyVersion("1.2.0.1010")]
 [assembly: ComVisible(false)]
 
 [assembly: SuppressMessage("Style", "IDE0290:Primären Konstruktor verwenden", Justification = "<Ausstehend>", Scope = "member", Target = "~M:POFF.Meet.Domain.PlayModes.RoundRobinPlayMode.#ctor(System.Int32)")]
